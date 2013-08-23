9bcfb78f89bf459b6c2dcc89dee7617106608ce9
Max Kovaljov <shirioko@gmail.com>
2013-08-23 12:44:47 +0200

    Added step2 reg form

---
+++ Dialogs/frmRegister2.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WinAppNET.Dialogs
+{
+    partial class frmRegister2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.btnSubmit = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(71, 64);
+            this.txtCode.MaxLength = 6;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.PasswordChar = '\0';
+            this.txtCode.PromptText = "Enter the code you received";
+            this.txtCode.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtCode.SelectedText = "";
+            this.txtCode.Size = new System.Drawing.Size(160, 23);
+            this.txtCode.TabIndex = 0;
+            this.txtCode.UseSelectable = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(24, 64);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(41, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Code";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(151, 94);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(80, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Verify";
+            this.btnSubmit.UseSelectable = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // frmRegister2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 138);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.txtCode);
+            this.Name = "frmRegister2";
+            this.Text = "Register code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox txtCode;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton btnSubmit;
+    }
+}
---
+++ Dialogs/frmRegister2.cs
@@ -0,0 +1,46 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinAppNET.Dialogs
+{
+    public partial class frmRegister2 : MetroForm
+    {
+        public string phonenumber;
+        public string identity;
+        public string password;
+
+        public frmRegister2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(this.txtCode.Text) && this.txtCode.Text.Length == 6)
+            {
+                try
+                {
+                    WhatsAppApi.Parser.PhoneNumber pn = new WhatsAppApi.Parser.PhoneNumber(this.phonenumber);
+                    this.password = WhatsAppApi.Register.WhatsRegisterV2.RegisterCode(pn.CC, pn.Number, this.txtCode.Text, this.identity);
+                    if (!string.IsNullOrEmpty(this.password))
+                    {
+                        this.DialogResult = System.Windows.Forms.DialogResult.OK;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Verification code not accepted");
+                    }
+                }
+                catch(Exception)
+                {}
+            }
+        }
+    }
+}
